cf9c871d129d8abf2cd7030e4e76fd83c88e2076
Mlor10 <mtlor@madisoncollege.edu>
2022-04-06 15:35:48 -0500

    Update DigimonCardResponse.java, added TODOs -Matthew Lor

--- src/main/java/project/entity/DigimonCardResponse.java
+++ src/main/java/project/entity/DigimonCardResponse.java
@@ -2,7 +2,29 @@ package project.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+// TODO: add relevant card info such as...
+//     "name": "MetalGreymon",
+//    "type": "Digimon",
+//    "color": "Yellow",
+//    "stage": "Ultimate",
+//    "attribute": "Virus",
+//    "level": null,
+//    "play_cost": null,
+//    "evolution_cost": null,
+//    "cardrarity": null,
+//    "artist": null,
+//    "dp": null,
+//    "cardnumber": "BO-01",
+//    "maineffect": null,
+//    "soureeffect": null,
+//    "set_name": "Series 1 Booster Pack",
+//    "card_sets": [
+//      "Series 1 Booster Pack"
+//    ]
+//    I thought about it and the point of this api is to combine and show card info and market price of the card,
+//    so we might as well add relevant info, be wary of null values though - Matthew Lor
+// TODO: Add properties to be displayed in results of index.jsp
+// TODO: Refactor toString() method with the new property variables
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DigimonCardResponse {
 
