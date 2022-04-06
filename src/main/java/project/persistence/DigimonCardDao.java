63aabcc966a852761625b2930c95c1a873b02de8
Mlor10 <mtlor@madisoncollege.edu>
2022-04-06 00:12:59 -0500

    Update DigimonCardDao.java, methods now work and retrieves response as an array -Matthew Lor

--- src/main/java/project/persistence/DigimonCardDao.java
+++ src/main/java/project/persistence/DigimonCardDao.java
@@ -7,6 +7,8 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.*;
 
@@ -16,38 +18,46 @@ import org.hibernate.SessionFactory;
 import project.digimoncard.DigimonCardResponse;
 
 public class DigimonCardDao {
-
     private final Logger logger = LogManager.getLogger(this.getClass());
+    private List<DigimonCardResponse> digimonCardResponseList;
 
+    public DigimonCardDao() {
+        digimonCardResponseList = new ArrayList<>();
+    }
 
     DigimonCardResponse getCardByName(String name) {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("https://digimoncard.io/api-public/search.php?n=" + name);
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         ObjectMapper mapper = new ObjectMapper();
-        DigimonCardResponse digimonCardResponse = null;
         try {
-            digimonCardResponse = mapper.readValue(response, DigimonCardResponse.class);
+            digimonCardResponseList = mapper.reader().forType(new TypeReference<List<DigimonCardResponse>>() {}).readValue(response);
         } catch (JsonProcessingException jsonProcessingException) {
             logger.error("Json Processing Exception: ", jsonProcessingException);
         }
-        return digimonCardResponse;
+        // TODO return a list of the result because there can be multiple variants of MetalGreymon cards
+        //  e.g.: put in MetalGreymon as input and all MetalGreymon searches show
+        //  Single value will result in an error since response returns an array
+        //  For Error Reference: Json Processing Exception: com.fasterxml.jackson.databind.exc.MismatchedInputException: Cannot deserialize instance of `project.digimoncard.DigimonCardResponse` out of START_ARRAY token
+        // This only works because MetalGreymon cardnumber BO-01 is the first in the list, Test would fail if
+        // it was anything other than .get(0) or the first index value
+        return digimonCardResponseList.get(0);
     }
 
     DigimonCardResponse getCardByNumber(String cardNumber) {
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://digimoncard.io/api-public/getAllCards.php");
+        WebTarget target = client.target("https://digimoncard.io/api-public/search.php?card=" + cardNumber);
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         ObjectMapper mapper = new ObjectMapper();
-        DigimonCardResponse digimonCardResponse = null;
         try {
-            digimonCardResponse = mapper.readValue(response, DigimonCardResponse.class);
+            digimonCardResponseList = mapper.reader().forType(new TypeReference<List<DigimonCardResponse>>() {}).readValue(response);
         } catch (JsonProcessingException jsonProcessingException) {
             logger.error("Json Processing Exception: ", jsonProcessingException);
-
         }
-
-        return digimonCardResponse;
+        logger.info(digimonCardResponseList);
+        // This will always work since each card number is unique
+        // as there are no two different cards with the same card number
+        return digimonCardResponseList.get(0);
     }
 
 //    Set<DigimonCardResponse> getAllCards() {
@@ -59,6 +69,7 @@ public class DigimonCardDao {
 //        Set<DigimonCardResponse> digimonCardResponse = null;
 //        try {
 //            // TODO figure out how to do with a collection
+//            // TODO reference list mapper object above to try and grab all the cards. Probably needs a container object class to hold and return all the cards
 //            digimonCardResponse = mapper.readValue(response, DigimonCardResponse.class);
 //        } catch (JsonProcessingException jsonProcessingException) {
 //            logger.error("Json Processing Exception: ", jsonProcessingException);
