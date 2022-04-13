1ef56de4cb72d0a54c4d760cf0f08935a17a6ff7
AshleyNWagner <anwagner@madisoncollege.edu>
2022-04-13 16:48:53 -0500

    Change DigimonCardDaoTest class to test new getCardByParameter method

--- src/test/java/project/persistence/DigimonCardDaoTest.java
+++ src/test/java/project/persistence/DigimonCardDaoTest.java
@@ -9,43 +9,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DigimonCardDaoTest {
-//    private final Logger logger = LogManager.getLogger(this.getClass());
-//    DigimonCardDao cardDao;
-//
-//    /**
-//     * Runs before each test
-//     * Refreshes the object
-//     */
-//    @BeforeEach
-//    void setUp() {
-//        cardDao = new DigimonCardDao();
-//    }
-//
-//    /**
-//     * Retrieves the card by name
-//     */
-//    @Test
-//    @Disabled
-//    void getCardByNameSuccess() {
-//        logger.info(cardDao.getCardByName("MetalGreymon").get(0).getCardNumber());
-//        assertEquals("BO-01", cardDao.getCardByName("MetalGreymon").get(0).getCardNumber());
-//    }
-//
-//    /**
-//     * Retrieves the card by number
-//     */
-//    @Test
-//    @Disabled
-//    void getCardByNumberSuccess() {
-//        logger.info(cardDao.getCardByNumber("BO-01").getCardNumber());
-//        assertEquals("BO-01", cardDao.getCardByNumber("BO-01").getCardNumber());
-//    }
-//
-//    /**
-//     * Retrieves all cards
-//     */
-//    @Test
-//    @Disabled
-//    void getAllCardsSuccess() {
-//    }
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    DigimonCardDao cardDao;
+
+    /**
+     * Runs before each test
+     * Refreshes the object
+     */
+    @BeforeEach
+    void setUp() {
+        cardDao = new DigimonCardDao();
+    }
+
+    /**
+     * Retrieves the card by name
+     */
+    @Test
+    void getCardByNameSuccess() {
+        logger.info(cardDao.getCardByParameter("n=", "MetalGreymon").get(0).getCardNumber());
+        assertEquals("BO-01", cardDao.getCardByParameter("n=","MetalGreymon").get(0).getCardNumber());
+    }
+
+    /**
+     * Retrieves the card by number
+     */
+    @Test
+    void getCardByNumberSuccess() {
+        logger.info(cardDao.getCardByParameter("card=","BO-01").get(0).getCardName());
+        assertEquals("A Blazing Storm of Metal!", cardDao.getCardByParameter("card=", "BT5-103").get(0).getCardName());
+    }
+
+    /**
+     * Retrieves all cards
+     */
+    @Test
+    void getAllCardsSuccess() {
+    }
 }
